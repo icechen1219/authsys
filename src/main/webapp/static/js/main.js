c8f8c6aa99da387c11cec0aeab8066bb6e12b2bb
icechen1219 <sagachen@aliyun.com>
2018-11-08 09:18:01 +0800

    fixed bug: 部署到服务器下后，出现菜单获取请求路径错误，忘记加工程路径导致。

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -7,24 +7,14 @@
 */
 var contextPath;
 
+$(function () {
+    console.log("main.js loading...")
+})
+
 function initPath(realPath) {
     contextPath = realPath;
-}
-
-$.parser.onComplete = function () {
-    // 获取选中的标签页面板（tab panel）和它的标签页（tab）对象
-    var pp = $('.easyui-tabs').tabs('getSelected');
-    var activeTab = pp&&pp.panel('options').tab; // 相应的标签页（tab）对象
-    var videoDiv = $(".video-js");
-    if (videoDiv.length !=0 && activeTab&&activeTab[0].textContent=='课件学习') {
-        console.log("video page loading...");
-        $.getScript(contextPath + "/static/js/play.js");
-    }
-}
-
-$(function () {
     $.ajax({
-        url: "/menuTree",
+        url: contextPath+"/menuTree",
         type: "get",
         dataType: "json",
         success: function (data) {
@@ -50,7 +40,19 @@ $(function () {
         }
 
     })
-})
+}
+
+$.parser.onComplete = function () {
+    // 获取选中的标签页面板（tab panel）和它的标签页（tab）对象
+    var pp = $('.easyui-tabs').tabs('getSelected');
+    var activeTab = pp&&pp.panel('options').tab; // 相应的标签页（tab）对象
+    var videoDiv = $(".video-js");
+    if (videoDiv.length !=0 && activeTab&&activeTab[0].textContent=='课件学习') {
+        console.log("video page loading...");
+        $.getScript(contextPath + "/static/js/play.js");
+    }
+}
+
 
 function opentabs(title, url) {
     if ($('.easyui-tabs').tabs('exists', title)) {
